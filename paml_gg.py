e312fe72ed60bb568580a9fa08301c63a04e6048
Luiza <luuhgomes98@gmail.com>
2022-03-30 20:49:12 -0300

    updated code

--- paml_gg.py
+++ paml_gg.py
@@ -119,6 +119,10 @@ def create_ligase() -> sbol3.Component:
     ligase.name = 'enzyme, bind sequences, needs buffer T4'   
     return ligase
 
+def create_purewater() -> sbol3.Component:
+    water = sbol3.Component('uater','https://github.com/BioArtBot/protocols/water')
+    water.name = 'pure water, fills up to 10 ul'   
+    return water
 
 # settle parameters for fuction definitions
 doc = prepare_document()
@@ -148,51 +152,65 @@ PREFIX_MAP = json.dumps({"cont": CONT_NS, "om": OM_NS})
 def provision_MC016(protocol: paml.Protocol, plate, plasmid_backbone):
     c_plasmid_backbone = protocol.primitive_step('PlateCoordinates', source=plate.output_pin('samples'), coordinates='A1:B1')
     protocol.primitive_step('Provision', resource=plasmid_backbone, destination=c_plasmid_backbone.output_pin('samples'),
-                            amount=sbol3.Measure(15, tyto.OM.microliter))
+                            amount=sbol3.Measure(5, tyto.OM.microliter))
     return c_plasmid_backbone
 
 def provision_MC043(protocol: paml.Protocol, plate, promorbs):
     c_promorbs = protocol.primitive_step('PlateCoordinates', source=plate.output_pin('samples'), coordinates='C1:D1')
     protocol.primitive_step('Provision', resource=promorbs, destination=c_promorbs.output_pin('samples'),
-                            amount=sbol3.Measure(15, tyto.OM.microliter))
+                            amount=sbol3.Measure(5, tyto.OM.microliter))
     return c_promorbs 
 
 def provision_MC066(protocol: paml.Protocol, plate, GFP):
     c_GFP = protocol.primitive_step('PlateCoordinates', source=plate.output_pin('samples'), coordinates='E1:F1')
     protocol.primitive_step('Provision', resource=GFP, destination=c_GFP.output_pin('samples'),
-                            amount=sbol3.Measure(15, tyto.OM.microliter))
+                            amount=sbol3.Measure(5, tyto.OM.microliter))
     return c_GFP
 
 def provision_MC024(protocol: paml.Protocol, plate, terminator):
     c_terminator = protocol.primitive_step('PlateCoordinates', source=plate.output_pin('samples'), coordinates='G1:H1')
     protocol.primitive_step('Provision', resource=terminator, destination=c_terminator.output_pin('samples'),
-                            amount=sbol3.Measure(15, tyto.OM.microliter))
+                            amount=sbol3.Measure(5, tyto.OM.microliter))
     return c_terminator
 
 def provision_bsaI(protocol: paml.Protocol, plate, bsaI):
     c_bsaI = protocol.primitive_step('PlateCoordinates', source=plate.output_pin('samples'), coordinates='A2:B2')
     protocol.primitive_step('Provision', resource=bsaI, destination=c_bsaI.output_pin('samples'),
-                            amount=sbol3.Measure(15, tyto.OM.microliter))
+                            amount=sbol3.Measure(5, tyto.OM.microliter))
     return c_bsaI
 
 def provision_bufferT4(protocol: paml.Protocol, plate, buffer):
     c_buffer = protocol.primitive_step('PlateCoordinates', source=plate.output_pin('samples'), coordinates='C2:D2')
     protocol.primitive_step('Provision', resource=buffer, destination=c_buffer.output_pin('samples'),
-                            amount=sbol3.Measure(15, tyto.OM.microliter))
+                            amount=sbol3.Measure(5, tyto.OM.microliter))
     return c_buffer
 
 def provision_ligase(protocol: paml.Protocol, plate, ligase):
     c_ligase = protocol.primitive_step('PlateCoordinates', source=plate.output_pin('samples'), coordinates='E2:F2')
     protocol.primitive_step('Provision', resource=ligase, destination=c_ligase.output_pin('samples'),
-                            amount=sbol3.Measure(15, tyto.OM.microliter))
+                            amount=sbol3.Measure(5, tyto.OM.microliter))
     return c_ligase
 
+def provision_water(protocol: paml.Protocol, plate, water):
+    c_water = protocol.primitive_step('PlateCoordinates', source=plate.output_pin('samples'), coordinates='G2:H2')
+    protocol.primitive_step('Provision', resource=water, destination=c_water.output_pin('samples'),
+                            amount=sbol3.Measure(10, tyto.OM.microliter))
+    return c_water
 
-# define the function for assembly
+# define the function for assembly/ transfering the materials for the soource wells to the build wells
 
-def assemble_components(protocol:paml.Protocol,source_wells,build_wells) -> None:
-    protocol.primitive_step('TransferByMap', source=source_wells, destination=build_wells.output_pin('samples'), 
-    plan=sbol3.Measure(2, tyto.OM.microliter))
+def assemble_rest(protocol:paml.Protocol,c_buffer,c_water,goldengate_build_wells) -> None:
+   
+    protocol.primitive_step('Transfer', source=c_buffer, destination=goldengate_build_wells.output_pin('samples'), 
+    amount=sbol3.Measure(2, tyto.OM.microliter), dispenseVelocity=sbol3.Measure(2, tyto.OM.microliter)) 
+
+    protocol.primitive_step('Transfer', source=c_water, destination=goldengate_build_wells.output_pin('samples'), 
+    amount=sbol3.Measure(4, tyto.OM.microliter), dispenseVelocity=sbol3.Measure(2, tyto.OM.microliter)) 
+
+
+def assemble_components(protocol, component, goldengate_build_wells):
+    protocol.primitive_step('Transfer', source=component, destination=goldengate_build_wells.output_pin('samples'), 
+    amount=sbol3.Measure(1, tyto.OM.microliter), dispenseVelocity=sbol3.Measure(2, tyto.OM.microliter))
 
 #############################################
  # set up the document
@@ -232,6 +250,9 @@ def golden_gate_protocol() -> Tuple[paml.Protocol, Document]:
     ligase = create_ligase()
     doc.add(ligase)
 
+    water = create_purewater()
+    doc.add(water)
+
 # actual steps of the protocol (liquid handling part)
 #get a plate
 
@@ -253,8 +274,10 @@ def golden_gate_protocol() -> Tuple[paml.Protocol, Document]:
 
     provision_ligase(protocol, plate, ligase)
 
+    provision_water(protocol, plate, water)
 
-     # list the materials for transfer function]
+
+     # list the materials for transfer function
 
     c_plasmid_backbone = provision_MC016(protocol, plate, plasmid_backbone) 
     c_promorbs = provision_MC043(protocol, plate, promorbs)
@@ -263,27 +286,21 @@ def golden_gate_protocol() -> Tuple[paml.Protocol, Document]:
     c_bsaI = provision_bsaI(protocol, plate, bsaI)
     c_buffer = provision_bufferT4(protocol, plate, buffer)
     c_ligase = provision_ligase(protocol, plate, ligase)
-
-    source_wells: list=[c_plasmid_backbone,c_promorbs,c_GFP,c_terminator,c_bsaI,c_buffer,c_ligase]
-
-
+    c_water = provision_water(protocol, plate, water)
+    
+   
 # define the wells where you will be doing the GG assembly  
-    goldengate_build_wells = protocol.primitive_step('PlateCoordinates', source=plate.output_pin('samples'), coordinates='G1:G2')
-
-    dna_source_wells = protocol.input_value('source_wells','http://bioprotocols.org/paml#SampleCollection')
-
-    dna_build_layout = protocol.input_value('goldengate_build_wells','http://bioprotocols.org/paml#SampleData') 
-
-    build_wells = protocol.primitive_step('DuplicateCollection', source=dna_build_layout) 
+    goldengate_build_wells = protocol.primitive_step('PlateCoordinates', source=plate.output_pin('samples'), coordinates='A3:B3')
 
 
 # assemble DNA in build wells
+    components: list = [c_plasmid_backbone,c_promorbs, c_GFP,c_terminator,c_bsaI,c_buffer, c_ligase, c_water]
+    for component in components:
+        assemble_components(protocol, component, goldengate_build_wells)
 
-    assemble_components(protocol,dna_source_wells,build_wells)  
-
+    assemble_rest(protocol,c_buffer,c_water,goldengate_build_wells)
 
 # Finish liquid handling protocol
-
 
     output = protocol.designate_output('constructs', 'http://bioprotocols.org/paml#SampleCollection',
     goldengate_build_wells.output_pin('samples'))
@@ -295,12 +312,11 @@ def golden_gate_protocol() -> Tuple[paml.Protocol, Document]:
 ########################################
 # Validate and write the document
 if __name__ == '__main__':
-    new_protocol: paml.Protocol
-    new_protocol, doc = golden_gate_protocol() 
+    protocol: paml.Protocol
+    protocol, doc = golden_gate_protocol() 
     print('Validating and writing protocol')
     v = doc.validate()
     assert len(v) == 0, "".join(f'\n {e}' for e in v)
-
 
 
 print('Validating and writing protocol')
@@ -316,4 +332,3 @@ print(f'Wrote file as {rdf_filename}')
 dot = protocol.to_dot()
 dot.render(f'{protocol.name}.gv')
 dot.view()
-
